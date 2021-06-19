adf46a0ba5348f9733896d6e72413d00d25da528
anaestheramaral <anaestheramaral@gmail.com>
2021-06-19 12:02:38 +0200

    chore: general style config

---
+++ pages/home.tsx
@@ -0,0 +1,25 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>fisioPront | Home</title>
+      </Head>
+      
+      <header>
+      <h1>
+          Olá Fulano!
+      </h1>
+      </header>
+      <main>
+        
+
+        (...)
+      </main>
+
+        
+    </div>
+  )
+}
