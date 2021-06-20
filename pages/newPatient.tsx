709ef5d705f1bd032e00d086529f1edff02eaa86
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-06-20 19:42:04 -0400

    chore: create content

--- pages/newPatient.tsx
+++ pages/newPatient.tsx
@@ -1,26 +1,30 @@
-import React, { ChangeEvent, useContext, useState } from 'react';
-import { PatientsContext } from '../contexts/patients.context';
-import styles from '../styles/NewPatient.module.scss';
-import Button from '../components/Button';
-import Link from 'next/link';
+import React, { ChangeEvent, useContext, useState } from "react";
+import { PatientsContext } from "../contexts/patients.context";
+import styles from "../styles/NewPatient.module.scss";
+import Button from "../components/Button";
+import Link from "next/link";
 
-import Input from '../components/Input';
-import Textarea from '../components/TextArea';
-import { FaChevronLeft as BackIcon } from 'react-icons/fa';
-import { HeaderMenu, Userinfo } from '../components';
+import Input from "../components/Input";
+import Textarea from "../components/TextArea";
+import { FaChevronLeft as BackIcon } from "react-icons/fa";
+import { HeaderMenu, Userinfo } from "../components";
 
 interface PatientProps {
   name: string;
   internationMotive: string;
   observations: string;
-};
+}
 
 export default function Home() {
-  const {handleAddNewPatient} = useContext(PatientsContext);
+  const { handleAddNewPatient } = useContext(PatientsContext);
 
-  const [newPatient, setNewPatient] = useState<PatientProps>({} as PatientProps)
-  
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+  const [newPatient, setNewPatient] = useState<PatientProps>(
+    {} as PatientProps
+  );
+
+  const handleChange = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
     setNewPatient({
       ...newPatient,
       [e.target.name]: e.target.value,
@@ -28,38 +32,45 @@ export default function Home() {
   };
 
   const handleSubmit = () => {
-    handleAddNewPatient(newPatient)
-  }
+    handleAddNewPatient(newPatient);
+  };
 
   return (
-      <main className={styles.main}>
-        
+    <main className={styles.main}>
+      <HeaderMenu title="Adicionar novo paciente" />
 
-        <HeaderMenu title="Adicionar novo paciente" />
+      <div className={styles.container}>
+        <form onSubmit={handleSubmit} className={styles.form}>
+          <div className={styles.inputWrapper}>
+            <Input
+              onChange={handleChange}
+              placeholder="Nome"
+              name="name"
+              label="Nome:"
+            />
+            <Input
+              onChange={handleChange}
+              placeholder="Motivo da internação"
+              name="name"
+              label="Motivo da internação:"
+            />
+          </div>
+          <div style={{ width: "100%" }}>
+            <Textarea
+              onChange={handleChange}
+              name="observations"
+              rows={6}
+              label="Motivo da internação"
+            />
+          </div>
 
-        <div className={styles.container}>
-              <form onSubmit={handleSubmit} className={styles.form}>
-                <div>
-                  <div className={styles.inputWrapper}>
-                    <Input onChange={handleChange} placeholder="Nome" name="name" label="Nome:"/>
-                    <Input onChange={handleChange} placeholder="Motivo da internação" name="name" label="Motivo da internação:"/>
-                  </div>
-                  <div style={{width: '100%'}}>
-                    <Textarea onChange={handleChange} name="observations" rows={6} label="Motivo da internação"/>
-                  </div>
+          <Button type="submit">
+            <strong>Salvar</strong>
+          </Button>
+        </form>
 
-
-                </div>
-
-              <Button type="submit"><strong>Salvar</strong></Button>
-            </form>
-
-            <Userinfo />
-        </div>
-        
-        
-      </main>
-
-        
-  )
+        <Userinfo />
+      </div>
+    </main>
+  );
 }
