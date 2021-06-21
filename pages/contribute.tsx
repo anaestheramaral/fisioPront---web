63c8fc69675996f6215216e5e1fb9c9981e68e21
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-06-20 21:42:40 -0400

    chore: send users

--- pages/contribute.tsx
+++ pages/contribute.tsx
@@ -58,58 +58,60 @@ const users = [
 const pages: React.FC = () => {
   const [session] = useSession();
 
-  return !session ? (
-    <Login />
-  ) : (
-    <div className={styles.main}>
-      <div className={styles.content}>
-        <HeaderMenu title="Contribua Conosco" />
-        <div className={styles.box}>
-          <div>
-            <div className={styles.titlePage}><p>Conheça nosso trabalho</p></div>
-            <ul className={styles.cards}>
-              {users.map((user) => {
-                return (
-                  <li className={styles.card}>
-                    <div className={styles.title}>
-                      <p>{user.name}</p>
-                    </div>
-                    <div className={styles.category}>
-                      <p>{user.category}</p>
-                    </div>
-                    <div className={styles.info}>
-                      <p>GitHub</p>
-                      <a target="_blank" href={user.github}>
-                        Acessar
-                      </a>
-                    </div>
-                    <div className={styles.info}>
-                      <p>Linkedin </p>
-                      <a target="_blank" href={user.linkedin}>
-                        Acessar
-                      </a>
-                    </div>
-                    <div className={styles.info}>
-                      <p>E-mail </p>
-                      <a target="_blank" href={`mailto:${user.email}`}>
-                        Enviar Email
-                      </a>
-                    </div>
-                    <div className={styles.info}>
-                      <p>Discord </p>
-                      <a href="#">
-                        {user.discord}
-                      </a>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
+  if (session) {
+    return (
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <HeaderMenu title="Contribua Conosco" />
+          <div className={styles.box}>
+            <div>
+              <div className={styles.titlePage}>
+                <p>Conheça nosso trabalho</p>
+              </div>
+              <ul className={styles.cards}>
+                {users.map((user) => {
+                  return (
+                    <li className={styles.card}>
+                      <div className={styles.title}>
+                        <p>{user.name}</p>
+                      </div>
+                      <div className={styles.category}>
+                        <p>{user.category}</p>
+                      </div>
+                      <div className={styles.info}>
+                        <p>GitHub</p>
+                        <a target="_blank" href={user.github}>
+                          Acessar
+                        </a>
+                      </div>
+                      <div className={styles.info}>
+                        <p>Linkedin </p>
+                        <a target="_blank" href={user.linkedin}>
+                          Acessar
+                        </a>
+                      </div>
+                      <div className={styles.info}>
+                        <p>E-mail </p>
+                        <a target="_blank" href={`mailto:${user.email}`}>
+                          Enviar Email
+                        </a>
+                      </div>
+                      <div className={styles.info}>
+                        <p>Discord </p>
+                        <a href="#">{user.discord}</a>
+                      </div>
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
+  return <Login />;
 };
 
 export default pages;
