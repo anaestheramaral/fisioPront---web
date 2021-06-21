6357c6d6068f69084b70cb717a6ce0d9805fc962
Emerson Garrido <emersongarrido@hotmail.com.br>
2021-06-20 22:14:13 -0400

    chore: send users

--- pages/scales.tsx
+++ pages/scales.tsx
@@ -1,10 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { Login } from "../components/Login";
 import { HeaderMenu } from "../components";
 import { PatientsContext } from "../contexts/patients.context";
 import { useSession, signOut } from "next-auth/client";
-import styles from '../styles/patients.module.scss';
-
+import styles from "../styles/scales.module.scss";
 
 export default function Scales() {
   const [session] = useSession();
@@ -20,55 +19,15 @@ export default function Scales() {
           <div className={styles.user_list}>
             <div className={styles.header_actions}>
               <div className={styles.title}>
-                <p>Todos os Pacientes</p>
+                <p>Todas as Escalas</p>
               </div>
-            </div>
-
-            <div className={styles.header_list}>
-              <ul>
-                <li>Paciente</li>
-                <li>Data de Entrada</li>
-                <li>Status</li>
-              </ul>
             </div>
 
             <div className={styles.list_users}>
               <ul>
-                {patients.length > 0 ? (
-                  <div>
-                    {patients.map((pacient) => {
-                      return (
-                        <li key={pacient.name}>
-                          <div className={styles.avatar_user}>
-                            <img src="/images/pacients.png" alt="" />
-                            <div className={styles.avatar_title}>
-                              <div>
-                                <p>{pacient.name}</p>
-                              </div>
-                              <div className={styles.user_contact}>
-                                <p>{pacient.contact}</p>
-                              </div>
-                            </div>
-                          </div>
-                          <div className={styles.user_date}>
-                            <p>{pacient.date}</p>
-                          </div>
-                          <div
-                            className={`${styles.user_status} ${
-                              styles[pacient.styles]
-                            }`}
-                          >
-                            <p>{pacient.status}</p>
-                          </div>
-                        </li>
-                      );
-                    })}
-                  </div>
-                ) : (
-                  <div className={styles.notPatients}>
-                    <p>Não existe pacientes cadastrados no momento</p>
-                  </div>
-                )}
+                <div className={styles.notPatients}>
+                  <p>Não existe escalas cadastrados no momento</p>
+                </div>
               </ul>
             </div>
           </div>
